58382aeeceb0d15f72b114213a9d6d7ac2243900
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-20 15:52:30 +0200

    add more option for casella_testo and is variable

--- js-paliedispari/palindromo/script.js
+++ js-paliedispari/palindromo/script.js
@@ -1,8 +1,15 @@
 const risposta = document.getElementById("div_risposta");
+let ripeti, checks = 0;
 
-function palindromo(){
-    const parola = document.getElementById("casella_testo").value.trim(); //rileva la parola e elimina gli spazi all'inio e alla fine per evitare errori dell'utente
-    let ripeti, checks = 0;
+function parolaPalindroma(){
+    const parola = document.getElementById("casella_testo").value.toLowerCase().split(" ").join("");
+    /*
+        la variabile "parola" quando viene rilevata viene anche trattata con:
+            1) tramite .toLowerCase() vengono portate tutte le parole minuscole;
+            2) Tramite .split(" ") e .join("") viene privata di tutti gli spazi, così che:
+                2a) Se l'utente ha sbagliato mettendo degli spazi prima o dopo non vengono contate;
+                2b) Se si tratta di una frase vengono tolti tutti gli spazi così da rendere la frase un'unica stringa così che gli spazi non diano fastidio all'algoritmo. 
+    */
 
     // controlla se la lunghezza della parola è pari o dispari; se la parola è pari allora modifica la variabile della lunghezza del ciclo con la metà della lunghezza della parola, altrimenti la lunghezza -1 per quelle dispasi visto che la lettera centrale non si ripeterà mai
     if(parola.length %= 2 == 0){ 
@@ -24,4 +31,5 @@ function palindromo(){
     } else {
         risposta.innerHTML = "No, non è palindroma";
     }
+    checks = 0; //questo pulisce la variabile se si vuole provare un altra parola
 }
